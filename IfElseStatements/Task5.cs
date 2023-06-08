2153da194badf64752a07b5305b5002d02d92655
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-06-08 16:14:05 +0400

    version 2

--- IfElseStatements/Task5.cs
+++ IfElseStatements/Task5.cs
@@ -76,98 +76,58 @@
 
         public static int DoSomething2(bool b1, bool b2, bool b3, bool b4)
         {
-            if (!b1 && !b2 && !b3)
+            int result;
+
+            if (b1 && b2)
             {
-                if (!b4)
-                {
-                    return 11;
-                }
-                else
-                {
-                    return 10;
-                }
+                result = 1;
             }
-            else if (b1 && !b2 && !b3)
+            else if (b1 && b3)
             {
-                if (!b4)
-                {
-                    return 0;
-                }
-                else
-                {
-                    return 3;
-                }
+                result = 2;
             }
-            else if (!b1 && b2 && !b3)
+            else if (b1 && b4)
             {
-                if (!b4)
-                {
-                    return 8;
-                }
-                else
-                {
-                    return 9;
-                }
+                result = 3;
             }
-            else if (b1 && b2 && !b3)
+            else if (!b1 && b3 && !b4 && b2)
             {
-                if (!b4)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 1;
-                }
+                result = 4;
             }
-            else if (!b1 && !b2 && b3)
+            else if (!b1 && b3 && !b4 && !b2)
             {
-                if (!b4)
-                {
-                    return 5;
-                }
-                else
-                {
-                    return 6;
-                }
+                result = 5;
             }
-            else if (b1 && !b2 && b3)
+            else if (!b1 && b3 && b4 && !b2)
             {
-                if (!b4)
-                {
-                    return 2;
-                }
-                else
-                {
-                    return 2;
-                }
+                result = 6;
             }
-            else if (!b1 && b2 && b3)
+            else if (!b1 && b3 && b4 && b2)
             {
-                if (!b4)
-                {
-                    return 4;
-                }
-                else
-                {
-                    return 7;
-                }
+                result = 7;
             }
-            else if (b1 && b2 && b3)
+            else if (!b1 && !b3 && b2 && !b4)
             {
-                if (!b4)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 1;
-                }
+                result = 8;
+            }
+            else if (!b1 && !b3 && b2 && b4)
+            {
+                result = 9;
+            }
+            else if (!b1 && !b3 && !b2 && b4)
+            {
+                result = 10;
+            }
+            else if (!b1 && !b3 && !b2 && !b4)
+            {
+                result = 11;
             }
             else
             {
-                return -1;
+                result = 0;
             }
+
+            return result;
         }
     }
 }
